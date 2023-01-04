69cb8a829659e1f67ca80ef36fe7195cf88d7ed6
Oleksii <oleksii@24slides.com>
2023-01-05 00:47:50 +0200

    Created basic navigation between form and products

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Form } from "./components/Form/Form";
 import { Header } from "./components/Header/Header";
+import { ProductList } from "./components/ProductList/ProductList";
 import { useTelegram } from "./hooks/useTelegram";
 
 function App() {
-  const { onToggleButton, telegram } = useTelegram();
+  const { telegram } = useTelegram();
 
   useEffect(() => {
     telegram.ready();
@@ -13,7 +16,10 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <button onClick={onToggleButton}>Toggle</button>
+      <Routes>
+        <Route index element={<ProductList />}></Route>
+        <Route path={"form"} element={<Form />}></Route>
+      </Routes>
     </div>
   );
 }
